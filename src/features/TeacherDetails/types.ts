680ba89d7f75c2f92fcc0d96ca52e4acb13455ed
Petro Cardoso <petrolcds@gmail.com>
2022-10-23 01:19:01 -0300

    :construction: wip: Pesquisa de professores

---
+++ src/features/TeacherDetails/types.ts
@@ -0,0 +1,23 @@
+export interface Horario {
+  Quinta?: Array<string[]>;
+  Sexta?: Array<string[]>;
+}
+
+export interface Turma {
+  turma: string;
+  local: string;
+  horarios: Horario[];
+}
+
+export interface Disciplina {
+  disciplina: string;
+  turmas: Turma[];
+}
+
+export interface Docente {
+  _id: string;
+  docente: string;
+  disciplinas: {[code: string]: Disciplina};
+  createdAt: string;
+  __v: number;
+}
